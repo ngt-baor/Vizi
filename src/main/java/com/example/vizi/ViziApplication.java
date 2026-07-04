323660b7727fe763317ddd87ee44b45acb12e46a
Baor-05 <baontph51745@gmail.com>
2026-07-04 11:39:00 +0700

    the beginning

---
+++ src/main/java/com/example/vizi/ViziApplication.java
@@ -0,0 +1,13 @@
+package com.example.vizi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViziApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vizi/ViziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vizi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
